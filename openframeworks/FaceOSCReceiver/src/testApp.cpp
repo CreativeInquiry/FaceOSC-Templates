abec14f4f915e4eab0fa454826d998bdc9d758a7
Dan Moore <dan@makeitdoathing.com>
2016-09-30 11:19:27 -0400

    updates

--- openframeworks/FaceOSCReceiver/src/testApp.cpp
+++ openframeworks/FaceOSCReceiver/src/testApp.cpp
@@ -52,16 +52,16 @@ void testApp::draw() {
             
             ofSetColor(0);
             ofNoFill();
-            ofEllipse(-20, face.eyeLeft * -9, 20, 7);
-            ofEllipse(20, face.eyeRight * -9, 20, 7);
-            ofEllipse(0, 20, face.mouthWidth * 3, face.mouthHeight * 3);
-            ofEllipse(-5, face.nostrils * -1, 7, 3);
-            ofEllipse(5, face.nostrils * -1, 7, 3);
+            ofDrawEllipse(-20, face.eyeLeft * -9, 20, 7);
+            ofDrawEllipse(20, face.eyeRight * -9, 20, 7);
+            ofDrawEllipse(0, 20, face.mouthWidth * 3, face.mouthHeight * 3);
+            ofDrawEllipse(-5, face.nostrils * -1, 7, 3);
+            ofDrawEllipse(5, face.nostrils * -1, 7, 3);
             
             ofSetRectMode(OF_RECTMODE_CENTER);
             ofFill();
-            ofRect(-20, face.eyebrowLeft * -5, 25, 5);
-            ofRect(20, face.eyebrowRight * -5, 25, 5);
+            ofDrawRectangle(-20, face.eyebrowLeft * -5, 25, 5);
+            ofDrawRectangle(20, face.eyebrowRight * -5, 25, 5);
             ofSetRectMode(OF_RECTMODE_CORNER);
         ofPopMatrix();
     }
